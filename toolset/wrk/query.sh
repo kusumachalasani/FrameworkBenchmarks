a19170577c82a73196ae6643b10fa0de053d8b05
kusumachalasani <kchalasa@redhat.com>
2021-09-13 06:26:10 -0400

    add wrk2 for load simulation to quarkus13_2x branch
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- toolset/wrk/query.sh
+++ toolset/wrk/query.sh
@@ -1,35 +1,73 @@
 #!/bin/bash
 
 let max_threads=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+echo "---------------------------------------------------------"
+echo "Removing any old hyperfoil logs."
+rm -rf /tmp/hyperfoil/run/*
+echo "---------------------------------------------------------"
+
 echo ""
 echo "---------------------------------------------------------"
 echo " Running Primer $name"
-echo " /wrk/hyperfoil-0.13/bin/wrk2.sh -H 'Host: $server_host' -H 'Accept: $accept' -H 'Connection: keep-alive' --latency -d 5 -c 8 --timeout 8 -t 8 --rate 10000 \"${url}2\""
+echo " /wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d 5 -c 8 --timeout 8 -t 8 --rate 10000 \"${url}2\""
 echo "---------------------------------------------------------"
 echo ""
-/wrk/hyperfoil-0.13/bin/wrk2.sh -H "Host: $server_host" -H "Accept: $accept" -H "Connection: keep-alive" --latency -d 5 -c 8 --timeout 8 -t 8 --rate 10000 "${url}2"
+/wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d 5 -c 8 --timeout 8 -t 8 --rate 10000 "${url}2"
 sleep 5
+
+echo "---------------------------------------------------------"
+echo "Listing the stats files. If file size is 214KB , then no blocked connection issue."
+ls -lrt /tmp/hyperfoil/run/*/stats/failures.csv
+echo "---------------------------------------------------------"
+
+echo "---------------------------------------------------------"
+echo "Removing any old hyperfoil logs."
+rm -rf /tmp/hyperfoil/run/*
+echo "---------------------------------------------------------"
+
 
 echo ""
 echo "---------------------------------------------------------"
 echo " Running Warmup $name"
-echo " /wrk/hyperfoil-0.13/bin/wrk2.sh -H 'Host: $server_host' -H 'Accept: $accept' -H 'Connection: keep-alive' --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 \"${url}2\""
+echo " /wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 \"${url}2\""
 echo "---------------------------------------------------------"
 echo ""
-/wrk/hyperfoil-0.13/bin/wrk2.sh -H "Host: $server_host" -H "Accept: $accept" -H "Connection: keep-alive" --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 "${url}2"
+/wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 "${url}2"
 sleep 5
+
+echo "---------------------------------------------------------"
+echo "Listing the stats files. If file size is 214KB , then no blocked connection issue."
+ls -lrt /tmp/hyperfoil/run/*/stats/failures.csv
+echo "---------------------------------------------------------"
+
+echo "---------------------------------------------------------"
+echo "Removing any old hyperfoil logs."
+rm -rf /tmp/hyperfoil/run/*
+echo "---------------------------------------------------------"
+
 
 for c in $levels
 do
 echo ""
 echo "---------------------------------------------------------"
 echo " Queries: $c for $name"
-echo " /wrk/hyperfoil-0.13/bin/wrk2.sh -H 'Host: $server_host' -H 'Accept: $accept' -H 'Connection: keep-alive' --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 \"$url$c\""
+echo " /wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 \"$url$c\""
 echo "---------------------------------------------------------"
 echo ""
 STARTTIME=$(date +"%s")
-/wrk/hyperfoil-0.13/bin/wrk2.sh -H "Host: $server_host" -H "Accept: $accept" -H "Connection: keep-alive" --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 "$url$c"
+/wrk/hyperfoil-0.13/bin/wrk2.sh --latency -d $duration -c $max_concurrency --timeout 8 -t $max_threads --rate 10000 "$url$c"
 echo "STARTTIME $STARTTIME"
 echo "ENDTIME $(date +"%s")"
 sleep 2
 done
+
+echo "---------------------------------------------------------"
+echo "Listing the stats files. If file size is 214KB , then no blocked connection issue."
+ls -lrt /tmp/hyperfoil/run/*/stats/failures.csv
+echo "---------------------------------------------------------"
+
+echo "---------------------------------------------------------"
+echo "Removing any old hyperfoil logs."
+rm -rf /tmp/hyperfoil/run/*
+echo "---------------------------------------------------------"
